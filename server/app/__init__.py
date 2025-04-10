0bf83d802da5f6082cb43abadcd4b868e15d0aa3
ROY <101548778+Nightcatclub@users.noreply.github.com>
2025-04-11 04:40:20 +1000

    fix problem

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)  # Enable CORS for all routes
+    CORS(app, resources={r"/*": {"origins": "https://fit5120-green.pages.dev"}}, supports_credentials=True)
     from .routes.green_score import green_score_bp, chat_bp
     app.register_blueprint(green_score_bp)
     app.register_blueprint(chat_bp)

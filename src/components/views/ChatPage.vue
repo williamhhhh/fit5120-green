89905fe1169caa8dddd70e87c15b09eaec6a5de2
ROY <101548778+Nightcatclub@users.noreply.github.com>
2025-04-10 20:06:27 +1000

    Update ChatPage.vue

--- src/components/views/ChatPage.vue
+++ src/components/views/ChatPage.vue
@@ -86,7 +86,7 @@ export default {
             this.loading = true;
 
             try {
-                const response = await axios.post("http://127.0.0.1:5000/", {
+                const response = await axios.post("http://127.0.0.1:5000/chat", {
                     prompt: userMessage,
                 });
 
